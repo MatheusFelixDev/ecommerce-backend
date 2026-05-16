b74b8abd01b198aece66052845e0104b01b0dc9b
Matheus Felix <matheusfelix.dev@gmail.com>
2026-05-16 02:08:44 -0300

    chore: add mercado pago env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -29,4 +29,36 @@ export const env = {
 
   melhorEnvioOriginZipCode:
     process.env.MELHOR_ENVIO_ORIGIN_ZIP_CODE || "",
+
+  mercadoPagoEnabled:
+    process.env.MERCADO_PAGO_ENABLED === "true",
+
+  mercadoPagoBaseUrl:
+    process.env.MERCADO_PAGO_BASE_URL ||
+    "https://api.mercadopago.com",
+
+  mercadoPagoAccessToken:
+    process.env.MERCADO_PAGO_ACCESS_TOKEN || "",
+
+  mercadoPagoPublicKey:
+    process.env.MERCADO_PAGO_PUBLIC_KEY || "",
+
+  mercadoPagoWebhookSecret:
+    process.env.MERCADO_PAGO_WEBHOOK_SECRET || "",
+
+  mercadoPagoSuccessUrl:
+    process.env.MERCADO_PAGO_SUCCESS_URL ||
+    "http://localhost:3000/payments/success",
+
+  mercadoPagoFailureUrl:
+    process.env.MERCADO_PAGO_FAILURE_URL ||
+    "http://localhost:3000/payments/failure",
+
+  mercadoPagoPendingUrl:
+    process.env.MERCADO_PAGO_PENDING_URL ||
+    "http://localhost:3000/payments/pending",
+
+  mercadoPagoNotificationUrl:
+    process.env.MERCADO_PAGO_NOTIFICATION_URL ||
+    "http://localhost:3000/api/payments/webhooks/mercado-pago",
 };
